6c714f7c342ad955fcdab1593e7c5cf5b18539c0
jones01996345 <96329808+jones01996345@users.noreply.github.com>
2022-05-14 21:11:24 +0800

    update_更新頁面

--- Appedu_JRPG/Assets/menu/SceneControl.cs
+++ Appedu_JRPG/Assets/menu/SceneControl.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void DelayLoadScene()
     {
-        
+        Invoke("LoadScene", 1.5f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene()
     {
-        
+        SceneManager.LoadScene("testing scenarios");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void DelayQuitGame()
+    {
+        Invoke("QuitGame", 1.5f);
     }
 }
